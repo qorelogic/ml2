f6f1190f86f82a6cc01a44f856df2f2236b6d9ec
jeff kiiza <kiizaa@gmail.com>
2015-01-17 19:49:12 -0300

    quandl-fun AUD pairs, EUR, USD, AUD visualization, refactoring

--- bin/qoreliquid.py
+++ bin/qoreliquid.py
@@ -26,3 +26,34 @@ def sigmoidme(dfr):
 def sharpe(dfr):
     ''
 """
+
+# source: https://www.quandl.com/c/markets/exchange-rates-versus-eur
+# quandl js parser: for (var i = 2; i<=15; i++) {var buff = ''; $($x('//*[@id="ember894"]/div['+i+']/table/tbody/tr/td[3]/a/@href')).each(function(e,o) {buff += ' '+o.value.replace(/\/CURRFX\//g, '');}); console.log('# '+i); console.log('pa += \''+buff+'\'');}
+
+# source: https://www.quandl.com/c/usa/usa-currency-exchange-rate
+# quandl js parser: for (var i = 2; i<=15; i++) {var buff = ''; $($x('//*[@id="ember894"]/div['+i+']/table/tbody/tr/td[3]/a/@href')).each(function(e,o) {buff += ' '+o.value.replace(/\/CURRFX\//g, '');}); console.log('# '+i); console.log('pa += \''+buff+'\'');}
+
+# getDataFromQuandl(pa, curr)
+def getDataFromQuandl(pa, curr): # curr = EUR || USD, etc.
+    pa = pa.split(' ')
+    
+    tk = []
+    tl = []
+    for i in pa:
+        tk.append('BNP/'+i)
+        tl.append('BNP.'+i+' - '+i[0:3]+'/'+i[3:6])
+    #print tk
+    #print tl
+    
+    try:
+        fname = 'data/quandl/BNP.'+curr+'.csv'
+        d = p.read_csv(fname)
+    except IOError, e:
+        d = q.get(tk, authtoken="WVsyCxwHeYZZyhf5RHs2")        
+        #print d
+        d.to_csv('data/quandl/BNP.'+curr+'.csv')
+        print e
+        
+    #plot(d.ix[:,tl])
+    
+    return d
