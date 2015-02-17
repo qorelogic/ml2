002113a7220d623999ae6cbfe7cf06a67f27cac9
jeff kiiza <kiizaa@gmail.com>
2015-02-17 19:52:11 -0300

    quickPlot() mod airfares.py: flask server works cleanup test_flops.py micro-finance.ipynb quandl-fun2.ipynb

--- bin/airfares.py
+++ bin/airfares.py
@@ -4,36 +4,31 @@
 # <codecell>
 
 import flask
+import json as j
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def test():
-    return 'everything is running'
+    return 'everything is running\n'
 
-#@app.route('/high/poverty/stastes')
-#def high_poverty_states():
-#    donors_choose_url = 'http://'
+@app.route('/high/poverty/stastes')
+def high_poverty_states():
+	donors_choose_url = 'http://'
+
+# <codecell>
+
+@app.route('/rome2rio')
+def rome2rio():
+	import urllib2
+	#url = 'http://<server>/api/1.2/xml/Autocomplete?key=<key>&query=cinque'
+	url = 'http://free.rome2rio.com/api/1.2/json/Search?key=7L3e3oOO&oName=London&dName=Paris'
+	#url = 'http://google.com'
+	response = urllib2.urlopen(url)
+	html = response.read()
+	ret = j.loads(html)
+	#print ret
+	return html
 
 if __name__ == "__main__":
-    app.run()
-
-# <codecell>
-
-import json as j
-j.
-
-# <codecell>
-
-import urllib2
-#url = 'http://<server>/api/1.2/xml/Autocomplete?key=<key>&query=cinque'
-url = 'http://free.rome2rio.com/api/1.2/json/Search?key=7L3e3oOO&oName=London&dName=Paris'
-#url = 'http://google.com'
-response = urllib2.urlopen(url)
-html = response.read()
-ret = j.loads(html)
-print ret[]
-
-# <codecell>
-
-
+	app.run()
