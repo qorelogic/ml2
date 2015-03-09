3ef8de26581380d92c213ff214511256cd665516
jeff kiiza <kiizaa@gmail.com>
2015-03-09 15:42:18 -0300

    datafeeds: all instruments, tmuxifier

--- bin/datafeeds/tmuxifier/layouts/datafeeds.window.sh
+++ bin/datafeeds/tmuxifier/layouts/datafeeds.window.sh
@@ -1,6 +1,6 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-window_root "/ml.dev/bin/datafeeds"
+window_root "/ml.dev/bin/data/oanda/datafeed"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
@@ -11,12 +11,12 @@ split_v 50
 split_h 50 0
 split_h 50 1
 split_v 50 1
+split_v 50 3
 
 # Run commands.
 qrun_cmd() {
 run_cmd "p" $2
-#run_cmd "cd /ml.dev/bin/datafeeds" $2
-run_cmd "python oanda.py $1" $2
+run_cmd "tail -f $1.csv" $2
 }
 
 qrun_cmd EUR_USD 0
@@ -25,10 +25,11 @@ qrun_cmd GBP_USD 2
 qrun_cmd USD_CAD 3
 
 send_keys "watch -n1 'ls -lt /ml.dev/bin/data/oanda/datafeed'" 4
+send_keys "cd /ml.dev/bin/datafeeds/ && python oanda.py" 5
 
 # Paste text
 #send_keys "top"    # paste into active pane
 #send_keys "date" 1 # paste into pane 1
 
 # Set active pane.
-select_pane 3
+select_pane 4
