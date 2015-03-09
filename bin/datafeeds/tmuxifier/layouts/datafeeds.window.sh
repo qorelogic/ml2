36a7133f4c5ee251b1852d96dcbf6616840d5b33
jeff kiiza <kiizaa@gmail.com>
2015-03-09 14:45:30 -0300

    datafeed:  - a python process is spawned with every instrument,
    added filestats pane in tmuxifier datafeeds window

--- bin/datafeeds/tmuxifier/layouts/datafeeds.window.sh
+++ bin/datafeeds/tmuxifier/layouts/datafeeds.window.sh
@@ -10,6 +10,7 @@ new_window "datafeeds"
 split_v 50
 split_h 50 0
 split_h 50 1
+split_v 50 1
 
 # Run commands.
 qrun_cmd() {
@@ -23,9 +24,11 @@ qrun_cmd USD_JPY 1
 qrun_cmd GBP_USD 2
 qrun_cmd USD_CAD 3
 
+send_keys "watch -n1 'ls -lt /ml.dev/bin/data/oanda/datafeed'" 4
+
 # Paste text
 #send_keys "top"    # paste into active pane
 #send_keys "date" 1 # paste into pane 1
 
 # Set active pane.
-#select_pane 0
+select_pane 3
