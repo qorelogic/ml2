fb33411fce96d74dd8652f1b76814b0f6b223ee3
jeff kiiza <kiizaa@gmail.com>
2015-03-19 00:02:03 -0300

    prepTestDataFrame()  - unit tests

--- bin/test_qore.py
+++ bin/test_qore.py
@@ -3,6 +3,7 @@
 from nose.tools import *
 from qore import *
 import numpy as n
+import pandas as p
 
 def test_answer():
     ''
@@ -10,3 +11,5 @@ def test_answer():
     #assert fetchURL('http://www.google.com') == "test"
     #assert lynxDump2('http://www.google.com') == "test"
     #assert mkdir_p('/tmp/testdir123') == "test"
+    df = p.DataFrame([['a','b','c'],['buy','sell','sell'],[1,2,3]], index=['pair', 'bias', 'amount']).transpose()
+    assert prepTestDataFrame(df) == ['a', 'buy', 1, 'b', 'sell', 2, 'c', 'sell', 3]
