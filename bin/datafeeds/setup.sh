1509517efa9199f0e0cf07512ed3210946e86b91
liquid-rc04 <liquid-rc04@liquid>
2015-08-21 04:21:52 -0400

    tmuxifier setup qlm bugfix

--- bin/datafeeds/setup.sh
+++ bin/datafeeds/setup.sh
@@ -23,8 +23,11 @@ minstall() {
 	#if [ -f $MPWD/tmuxifier/layouts/datafeeds.window.sh ]; then
 	if [ ! -f $mlocal/layouts/datafeeds.window.sh ]; then
 		ln -s $MPWD/tmuxifier/layouts/datafeeds.window.sh $mlocal/layouts/ 2> /dev/null
-		ln -s $MPWD/tmuxifier/layouts/qlm.window.sh $mlocal/layouts/ 2> /dev/null
 		echo 'linked datafeeds tmuxifier layout.'
+	fi
+	if [ ! -f $mlocal/layouts/qlm.window.sh ]; then
+		ln -s $MPWD/../tmuxifier/layouts/qlm.window.sh $mlocal/layouts/ 2> /dev/null
+		echo 'linked qlm tmuxifier layout.'
 	fi
 	if [ ! -f $mldir/lib/oanda/oandapy/oandapy.py ]; then
 		mkdir -p $mldir/lib/oanda/
