3bcb2bbdb758c9e433b0c61e74298e8f5197bdef
liquid-rc04 <liquid-rc04@liquid>
2015-08-30 09:22:28 -0400

    provisioner:  - sparkling-water puppet install

--- bin/provisioner/default.pp
+++ bin/provisioner/default.pp
@@ -5,9 +5,11 @@ $hdir        = "/mldev"
 $installHdir = "$hdir/lib/ml"
 $h2oHdir     = "$installHdir/h2o"
 $sparkHdir   = "$installHdir/spark"
+$sparklingWaterHdir   = "$installHdir/spark"
 
 $h2oTarball   = "http://h2o-release.s3.amazonaws.com/h2o/rel-simons/7/h2o-3.0.1.7.zip"
 $sparkTarball = "http://d3kbcqa49mib13.cloudfront.net/spark-1.4.0-bin-hadoop2.4.tgz"
+$sparklingWaterTarball = "http://h2o-release.s3.amazonaws.com/sparkling-water/rel-1.4/3/sparkling-water-1.4.3.zip"
 
 class system-update {
   exec { 'apt-get update':
@@ -72,6 +74,34 @@ class h2o {
 	#exec { 'run h2o':      command => "java -jar $h2oHdir/h2o-3.0.1.7/h2o.jar",      timeout => 5, tries   => 3 }
 }
 
+class sparkling-water {
+	exec { "mkdir $sparklingWaterHdir": command => "mkdir -p $sparklingWaterHdir" }
+	exec { "bashrc touch":
+		command => "echo \"export SPARK_HOME='/home/qore/mldev/lib/ml/spark/spark-1.4.0-bin-hadoop2.4'\" >> /home/qore/.bashrc",
+		before  => Exec["wget $sparklingWaterTarball"],
+	}
+	exec { "wget $sparklingWaterTarball":
+		command => "wget -nc $sparklingWaterTarball -P $sparklingWaterHdir/",
+		#command => "wget -nc $sparklingWaterTarball",
+		#cwd => "$sparklingWaterHdir",
+		timeout => 60,
+		tries   => 3,
+		#creates => "$sparklingWaterHdir/sparkling-water-1.4.3.zip",
+		#refreshonly => true,
+		#notify => Exec['unzip sparkling'],
+		before  => Exec["unzip sparkling"],
+	}
+	exec { 'unzip sparkling': 
+		command => "unzip -o $sparklingWaterHdir/sparkling-water-1.4.3.zip -d $sparklingWaterHdir/", 
+		#command => "/usr/bin/unzip -o $sparklingWaterHdir/sparkling-water-1.4.3.zip",
+		#cwd => "$sparklingWaterHdir",
+		timeout => 60, 
+		tries   => 3,
+		require => Class["unzip"],
+	}
+	#exec { 'run sparkling':      command => "$sparklingWaterHdir/sparkling-water-1.4.3/bin/sparkling-shell",      timeout => 5, tries   => 3 }
+}
+
 class spark {
 	exec { "mkdir -p $sparkHdir": command => "mkdir -p $sparkHdir" }
 	exec { "wget -nc $sparkTarball":
@@ -86,7 +116,7 @@ class spark {
 		tries   => 3,
 		#require => File["$sparkHdir/spark-1.4.0-bin-hadoop2.4.tgz"],
 	}
-	#exec { 'run h2o':      command => "java -jar $sparkHdir/spark-1.4.0-bin-hadoop2.4/bin/...",        timeout => 60, tries   => 3 }
+	#exec { 'run spark':      command => "$sparkHdir/spark-1.4.0-bin-hadoop2.4/bin/spark-shell",        timeout => 60, tries   => 3 }
 }
 
 #include apache
@@ -96,3 +126,4 @@ include unzip
 include javart
 include h2o
 include spark
+include sparkling-water
