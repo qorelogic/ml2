a84a85c5d624867981294a92722bb8ec8242dec2
jeff kiiza <kiizaa@gmail.com>
2015-08-29 21:13:22 -0300

    - initial vagrant intall

---
+++ bin/hpc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "`which vagrant`" == "" ]; then
+	wget --no-check-certificate -c https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_i686.deb -P /tmp/
+	sudo dpkg -i /tmp/vagrant_1.7.4_i686.deb
+fi
+if [ "`which virtualbox`" == "" ]; then
+	sudo apt-get install virtualbox
+fi
+
+# source: https://atlas.hashicorp.com/ubuntu/boxes/trusty32
+vagrant init ubuntu/trusty32 2> /dev/null
+vagrant up --provider virtualbox
