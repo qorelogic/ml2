99008b995fd75065ae0055cc02f0c95ae62afca3
jeff kiiza <kiizaa@gmail.com>
2015-08-31 02:21:31 -0300

    babysit mod

--- bin/tmuxifier/layouts/babysit.window.sh
+++ bin/tmuxifier/layouts/babysit.window.sh
@@ -12,12 +12,11 @@ split_v 50 1
 
 # Run commands.
 run_cmd ".gpull" 0
-run_cmd "" 0
+run_cmd "python oanda.py babysit" 0
 run_cmd ".gpull" 1
 run_cmd "htop" 2
 
-send_keys "python oanda.py babysit" 0
-send_keys "./push.sh" 1
+#send_keys "./push.sh" 1
 
 # Set active pane.
 select_pane 0
