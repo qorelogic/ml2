efa3b789b402ce22742e1169daaaa55456d8af7b
liquid-rc04 <liquid-rc04@liquid>
2015-09-10 00:57:39 -0400

    Dataminer: logging system

--- bin/dataminer.sh
+++ bin/dataminer.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-killall -9 python 
+hdir="/mldev"
+hlogs="$hdir/bin/data/infofeeds/logs"
+
+#killall -9 python 
 
 pcwd=`pwd`
 
-cd /mldev/lib/DataPipeline/
-/usr/bin/scrapy crawl investingTechnical --nolog
+mkdir -p $hlogs
+
+cd $hdir/lib/DataPipeline/
+
+echo '=========='  >> $hlogs/dataminer.log
+date               >> $hlogs/dataminer.log
+echo '=========='  >> $hlogs/dataminer.err.log
+date               >> $hlogs/dataminer.err.log
+
+/usr/bin/scrapy crawl investingTechnical --nolog \
+                  1>> $hlogs/dataminer.log \
+                  2>> $hlogs/dataminer.err.log
+
 cd $pcwd
-/mldev/bin/qlm.py ta
+
+# run it!
+$hdir/bin/qlm.py ta 1>> $hlogs/dataminer.log 2>> $hlogs/dataminer.err.log
+
+echo '=========='  >> $hlogs/dataminer.log
+echo '=========='  >> $hlogs/dataminer.err.log
+
+exit 0
