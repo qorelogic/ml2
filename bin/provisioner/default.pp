54e0eda3f31a1eb89b704e006e4fa0e2c1977973
jeff kiiza <kiizaa@gmail.com>
2015-09-20 19:24:37 -0300

    provisioner:  - modularized cassandra.pp

--- bin/provisioner/default.pp
+++ bin/provisioner/default.pp
@@ -11,17 +11,8 @@ $h2oTarball   = "http://h2o-release.s3.amazonaws.com/h2o/rel-simons/7/h2o-3.0.1.
 $sparkTarball = "http://d3kbcqa49mib13.cloudfront.net/spark-1.4.0-bin-hadoop2.4.tgz"
 $sparklingWaterTarball = "http://h2o-release.s3.amazonaws.com/sparkling-water/rel-1.4/3/sparkling-water-1.4.3.zip"
 
-class system-update {
-  exec { 'apt-get update':
-    command => 'apt-get update',
-  }
+import 'provisioner/cassandra.pp'
 
-  $sysPackages = [ "build-essential" ]
-  package { $sysPackages:
-    ensure => "installed",
-    require => Exec['apt-get update'],
-  }
-}
 
 class apache {
   package { "apache2":
@@ -49,35 +40,6 @@ class unzip {
     require => Class["system-update"],
   }
 }
-
-# source: http://docs.datastax.com/en/cassandra/2.1/cassandra/install/installDeb_t.html
-class cassandra {
-	exec { 
-		"AddDataStaxCommunityRepository2cassandra.sources.list":
-		command => 'echo "deb http://debian.datastax.com/community stable main" | tee -a /etc/apt/sources.list.d/cassandra.sources.list',
-		before  => Exec["AddDataStaxReposKey2aptitudeTrustedKeys"]
-	}
-	exec { 
-		"AddDataStaxReposKey2aptitudeTrustedKeys":
-                command => 'curl -L http://debian.datastax.com/debian/repo_key | apt-key add -',
-		before  => Exec["InstallCassandra"]
-	}
-	$xv = '9'
-	exec { 
-		"InstallCassandra":
-                command => "apt-get install dsc21=2.1.${xv}-1 cassandra=2.1.${xv}",
-		before  => Exec["InstallCassandraTools"],
-		require => Class["system-update"]
-	}
-	exec { 
-		## Optional utilities
-		"InstallCassandraTools":
-                command => "apt-get install cassandra-tools=2.1.${xv}",
-		require => Class["system-update"]
-	}
-}
-
-
 
 # source: http://stackoverflow.com/questions/11327582/puppet-recipe-installing-tarball
 class h2o {
