7fea0e9328a12a8789108ff65a3dcf8e00f4ee90
jeff kiiza <kiizaa@gmail.com>
2015-10-05 01:13:20 -0300

    tmuxifier:  - mongo: bugfix

--- bin/tmuxifier/layouts/mongo.window.sh
+++ bin/tmuxifier/layouts/mongo.window.sh
@@ -11,8 +11,8 @@ split_v 50
 #split_h 50 0
 
 # Run commands.
-run_cmd "sudo mongod --config=/etc/mongodb.conf" 0
-run_cmd "tail -f /var/log/mongodb/mongodb.log" 1
+run_cmd "sudo mongod --config=/etc/mongod.conf" 0
+run_cmd "tail -f /var/log/mongodb/mongod.log" 1
 
 # Paste text
 
