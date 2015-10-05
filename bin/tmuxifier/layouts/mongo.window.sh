f3cacd00d33d2eafc555a12de195b8ac7ba4ba7d
jeff kiiza <kiizaa@gmail.com>
2015-10-05 00:55:43 -0300

    tmuxifier:  - mongo window: bugfix

--- bin/tmuxifier/layouts/mongo.window.sh
+++ bin/tmuxifier/layouts/mongo.window.sh
@@ -4,24 +4,17 @@ window_root "/ml.dev/bin"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
-new_window "ml.dev"
+new_window "mongod"
 
 # Split window into panes.
 split_v 50
 #split_h 50 0
 
 # Run commands.
-#run_cmd "top"     # runs in active pane
-#run_cmd "date" 1  # runs in pane 1
-run_cmd "cd /ml.dev/bin" 0
-run_cmd "cd /ml.dev/bin" 1
-run_cmd "p" 0
-run_cmd "p" 1
-run_cmd "ls && git branch -v" 1
+run_cmd "sudo mongod --config=/etc/mongodb.conf" 0
+run_cmd "tail -f /var/log/mongodb/mongodb.log" 1
 
 # Paste text
-#send_keys "top"    # paste into active pane
-#send_keys "date" 1 # paste into pane 1
 
 # Set active pane.
-#select_pane 0
+select_pane 0
