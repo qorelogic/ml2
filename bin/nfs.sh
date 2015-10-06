c149e12154ac21206f58b6e07fc2a4c64bea03ea
liquid-rc04 <liquid-rc04@liquid>
2015-10-06 10:15:36 -0400

    nfs: nfsmount() mount multiple filesystems

--- bin/nfs.sh
+++ bin/nfs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#miaddr="104.131.163.27"
-miaddr="45.55.92.77"
+nfsmount() {
+	miaddr="$1"
+	sudo mkdir /mnt/$miaddr 2> /dev/null
+	
+	#ssh -L 3049:localhost:2049 -oStrictHostKeyChecking=no root@$miaddr
+	sudo mount -t nfs -o port=2049 $miaddr:/mldev/bin/data /mnt/$miaddr
+}
 
-sudo mkdir /mnt/$miaddr 2> /dev/null
-
-#ssh -L 3049:localhost:2049 -oStrictHostKeyChecking=no root@$miaddr
-sudo mount -t nfs -o port=2049 $miaddr:/mldev/bin/data /mnt/$miaddr
+nfsmount 45.55.92.77
+nfsmount 104.131.163.27
