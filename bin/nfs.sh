a81d72d911c3b71c6d3603fd09858efd82a1235b
liquid-rc04 <liquid-rc04@liquid>
2015-10-06 10:10:14 -0400

    nfs: excecutable

--- bin/nfs.sh
+++ bin/nfs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-miaddr="104.131.163.27"
+#miaddr="104.131.163.27"
+miaddr="45.55.92.77"
 
-mkdir /mnt/$miaddr
+sudo mkdir /mnt/$miaddr 2> /dev/null
 
 #ssh -L 3049:localhost:2049 -oStrictHostKeyChecking=no root@$miaddr
 sudo mount -t nfs -o port=2049 $miaddr:/mldev/bin/data /mnt/$miaddr
