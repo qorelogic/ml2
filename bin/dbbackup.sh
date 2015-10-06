4894cab6058ae7041c8ffe9f269831991e873456
liquid-rc04 <liquid-rc04@liquid>
2015-10-06 18:10:40 -0400

    nfs:  - nfsmount dbbackup:  - mod

--- bin/dbbackup.sh
+++ bin/dbbackup.sh
@@ -6,6 +6,13 @@ dbwot="ticks"
 #mongodump --host=127.0.0.1 --port=27017 -d ql -c "${colwot}" --out - | gzip > /mldev/bin/data/db/${colwot}-db.date +%F.gz
 #mongodump --host=127.0.0.1 --port=27017 -d ql -c "${colwot}" --out - | bzip -z > /mldev/bin/data/db/${colwot}-db.date +%F.bz2
 
-ipaddr="$1"
-#mongodump --host=127.0.0.1 --port=27017 -d ql -c "${colwot}" --out /mnt/$ipaddr/db/
-rsync -avP /var/lib/mongodb/ root@$ipaddr:/var/lib/mongodb/
+if [ "$1" == "" ]; then
+	echo "usage: <ipaddr>"
+else
+	ipaddr="$1"
+	#mongodump --host=127.0.0.1 --port=27017 -d ql -c "${colwot}" --out /mnt/$ipaddr/db/
+
+	#rsync -avP /var/lib/mongodb/ root@$ipaddr:/var/lib/mongodb/
+	sudo rsync -avP /var/lib/mongodb/ /mnt/$ipaddr/data/var-lib-mongodb/
+	echo "rsync -avP /var/lib/mongodb/ /mnt/$ipaddr/mongodb/"
+fi
--- bin/nfs.sh
+++ bin/nfs.sh
@@ -2,12 +2,16 @@
 
 nfsmount() {
 	miaddr="$1"
-	sudo mkdir /mnt/$miaddr 2> /dev/null
+	sudo mkdir -p /mnt/$miaddr/mongodb 2> /dev/null
+	sudo mkdir -p /mnt/$miaddr/data 2> /dev/null
 	
 	#ssh -L 3049:localhost:2049 -oStrictHostKeyChecking=no root@$miaddr
 	sudo umount /mnt/$miaddr
-	#sudo mount -t nfs -o port=2049 $miaddr:/mldev/bin/data /mnt/$miaddr
-	sudo mount -t nfs -o port=2049 $miaddr:/var/lib/mongodb /mnt/$miaddr
+	sudo umount /mnt/$miaddr/mongodb
+	sudo mount -t nfs -o port=2049 $miaddr:/var/lib/mongodb /mnt/$miaddr/mongodb
+
+	sudo umount /mnt/$miaddr/data
+	sudo mount -t nfs -o port=2049 $miaddr:/mldev/bin/data /mnt/$miaddr/data
 }
 
 nfsmount 45.55.92.77
