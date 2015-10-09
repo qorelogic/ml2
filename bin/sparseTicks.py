734ee5aac07f02d32e2b2db774e05ec0e134058e
jeff kiiza <kiizaa@gmail.com>
2015-10-09 10:07:32 -0300

    sparseTicks:  - sparseTicks2dim3(): optimization

--- bin/sparseTicks.py
+++ bin/sparseTicks.py
@@ -1,15 +1,30 @@
 
-import numpy as n
-import pandas as p
-import pymongo
-from matplotlib import pyplot as plt
-from pylab import rcParams
-#rcParams['figure.figsize'] = 20, 5
-import datetime as dd
+def imports():
+    import numpy as n
+    import pandas as p
+    import pymongo
+    from matplotlib import pyplot as plt
+    from pylab import rcParams
+    #rcParams['figure.figsize'] = 20, 5
+    import datetime as dd
+    
+    #import numpy as n
+    #import pandas as p
+    #import pymongo
+    #from matplotlib import pyplot as plt
+    #from pylab import rcParams
+    ##rcParams['figure.figsize'] = 20, 5
+    #from pylab import rcParams
+    #rcParams['figure.figsize'] = 20, 5
+    
+    #import datetime as dd
+    #import ujson as j
+#imports()
 
 ##################
 def normalizeme(dfr, pinv=False):
     
+    import numpy as n
     nmean = n.mean(dfr, axis=0)
     nstd = n.std(dfr, axis=0)
     #nmean = n.mean(dfr)
@@ -22,28 +37,36 @@ def normalizeme(dfr, pinv=False):
         return [dfr, nmean, nstd]
 
 def sigmoidme(dfr):
+    import numpy as n
     return 1.0 / (1 + pow(n.e,-dfr))
 
+def _oandaToTimestamp(ptime):
+    import datetime as dd
+    dt = dd.datetime.strptime(ptime, '%Y-%m-%dT%H:%M:%S.%fZ')
+    return (dt - dd.datetime(1970, 1, 1)).total_seconds() / dd.timedelta(seconds=1).total_seconds()
 def oandaToTimestamp(ptime):
     
-    def _oandaToTimestamp(ptime):
-        dt = dd.datetime.strptime(ptime, '%Y-%m-%dT%H:%M:%S.%fZ')
-        return (dt - dd.datetime(1970, 1, 1)).total_seconds() / dd.timedelta(seconds=1).total_seconds()
-        
     try:
         tstmp = _oandaToTimestamp(ptime)
     except Exception as e:
         tstmp = []
         for i in ptime: tstmp.append(_oandaToTimestamp(i))                
     return tstmp
-##########################
+##################
 
 #@profile
-def sparseTicks():
+def sparseTicks(num=2000):
+
+    import pymongo
+    #import pandas as p
+    from pandas import DataFrame as p_DataFrame
+    from matplotlib import pyplot as plt
+    from pylab import rcParams
+    
     # Connection to Mongo DB
     try:
         conn = pymongo.MongoClient(port=27017)
-        print "Connected successfully!!!"
+        #print "Connected successfully!!!"
     except pymongo.errors.ConnectionFailure, e:
         print "Could not connect to MongoDB: %s" % e 
         ''
@@ -51,9 +74,9 @@ def sparseTicks():
     
     #@profile
     def _pe(db, dfi, num=100):
-        df = p.DataFrame()
+        df = p_DataFrame()
         for i in db.ticks.find(dfi)[0:num]:
-            try: df = df.combine_first(p.DataFrame(i, index=[i['_id']]))
+            try: df = df.combine_first(p_DataFrame(i, index=[i['_id']]))
             except KeyError as e: ''
         return df
     
@@ -68,25 +91,72 @@ def sparseTicks():
             k = i.keys()
             v = i.values()
             lis.append(v)
-        return p.DataFrame(lis, columns=k)
+        return p_DataFrame(lis, columns=k)
 
     # sparse ticks
     db = conn.ql
-    dfi = {}
-    num=500
+    dfi = {}    
+    
     #df = _pe(db, dfi, , num=num)
     df = _pe2(db, dfi, num=num)
 
     df = df.drop_duplicates('time')
+    indexby = 'time'
     df['ts'] = oandaToTimestamp(df['time'])
-    df = df.pivot('ts', 'instrument', 'ask')
+    df = df.pivot(indexby, 'instrument', 'ask')
     df = df.bfill()
     df = df.ffill()
     df = df.convert_objects(convert_numeric=True)#.ix[:, 'AUD_CHF']
     df = normalizeme(df)
     df = sigmoidme(df)
-    print df
-    df.plot(legend=None); plt.show();
+    #print df
+    #df.plot(legend=None); plt.show();
     #plt.plot(df); plt.show();
+
+    return df
+
+# convert sparse ticks dataframe to 3D matrix: 
+#       the 3rd dimension composed of historical price of depth mdepth
+#@profile
+def sparseTicks2dim3(df, mdepth=200):
     
-sparseTicks()
+    import numpy as n
+    from pandas import DataFrame as p_DataFrame
+    
+    dfn = df.get_values()
+    #dir(dfn)
+
+    dfnl = dfn.shape[0]-mdepth
+    dfm = n.resize(n.zeros(dfnl * mdepth * dfn.shape[1]), (dfnl, mdepth, dfn.shape[1]))
+
+    print 'shape dfn: {0}'.format(dfn.shape)
+    print 'shape dfm: {0}'.format(dfm.shape)
+    #print 'shape dfn: {0}'.format(dfn[0:200].shape)
+    #print 'shape dfm: {0}'.format(dfm[0].shape)
+
+    #print dfm[0,0]
+    #print dfn#[0]
+    #print dfm
+
+    #print dfn[0:200]
+    for i in range(dfn.shape[0]-mdepth):
+        #print dfn[i:mdepth+i]
+        dfm[i] = dfn[i:mdepth+i]
+    #print dfm
+    #print 'shape dfn: {0}'.format(dfn.shape)
+    #print 'shape dfm: {0}'.format(dfm.shape)
+
+    #print dfm
+    #print dfm#[0]
+    #for i in range(10):
+    #    print dfm[i]
+#        print 
+        #p_DataFrame(dfm[i]).plot(legend=False)
+    
+    return dfm
+
+##################
+##########################
+
+df = sparseTicks(num=2000)
+sparseTicks2dim3(df, mdepth=200)
