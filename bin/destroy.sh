c5a38beef72e413aca4de3faa8be9047d16742e9
liquid-rc04 <liquid-rc04@liquid>
2015-10-10 00:33:13 -0400

    destroy nodes

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-wipe -fqQ10 datafeeds/config.csv
-wipe -rfqQ10 data/oanda/logs/
-wipe -rfqQ10 /root/.bash_history
-wipe -rfqQ10 /hoome/qore/.bash_history
+sudo wipe -fqQ10 datafeeds/config.csv
+sudo wipe -rfqQ10 data/oanda/logs/
+sudo wipe -rfqQ10 /root/.bash_history
+sudo wipe -rfqQ10 /hoome/qore/.bash_history
+sudo wipe -rfqQ10 *.ipynb
+sudo wipe -rfqQ10 */*.ipynb
+sudo wipe -rfqQ10 */*.py
