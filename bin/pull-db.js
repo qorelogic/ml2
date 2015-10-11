af85cf6d6ff3492c43aa8a9f99f704b2af22caa0
jeff kiiza <kiizaa@gmail.com>
2015-10-11 08:35:01 -0300

    mongopull:  - pull-db.js code merged into pull-db.sh

--- bin/pull-db.js
+++
@@ -1,5 +0,0 @@
-
-use ql22b;
-db.dropDatabase();
-use ql22;
-db.copyDatabase("ql22","ql22b","localhost:27018");
--- bin/pull-db.sh
+++ bin/pull-db.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
-srcdb="ql22"
-dstdb="ql22b"
+srcdb="mydb2"
+dstdb="mydb234"
 dsthost="localhost"
 dstuser="qore2"
+portmongo=27017
+portlocal=27018
 
-#mongodump --db=$srcdb --tar --out=-
 #mongodump --db=$srcdb --tar --out=- | bzip2 --fast | ssh ${dstuser}@${dsthost} 'bunzip2 | mongorestore --db=$dstdb --drop --tar --dir=-'
 
-# source: http://stackoverflow.com/questions/16619598/sync-mongodb-via-ssh
-ssh -L27018:localhost:27017 $dsthost '
-    echo "Connected on Remote End, sleeping for 10"; 
-    sleep 10; 
-    exit' &
-echo "Waiting 5 sec on local";
-sleep 5;
-echo "Connecting to Mongo and piping in script";
-cat pull-db.js | mongo
+extractdb() {
+	dbfname="$1"; 
+	mkdir $dbfname; 
+	tar jxfv $dbfname.tar.bz2 #-C $dbfname
+}
 
+extract-all() {
+	extractdb "db2"
+	extractdb "db3"
+	extractdb "db4"
+	extractdb "db5"
+	extractdb "db6"
+}
+
+pull-db() {
+	# source: http://stackoverflow.com/questions/16619598/sync-mongodb-via-ssh
+	ssh -L$portlocal:localhost:$portmongo $dsthost '
+	    echo "Connected on Remote End, sleeping for 10"; 
+	    sleep 10; 
+	    exit' &
+	echo "Waiting 5 sec on local";
+	sleep 5;
+	echo "Connecting to Mongo and piping in script";
+	echo "
+	use $dstdb;
+	db.dropDatabase();
+	use $srcdb;
+	db.copyDatabase(\"$srcdb\",\"$dstdb\",\"localhost:$portlocal\");
+	" | mongo --verbose
+}
+
+#extract-all
+#pull-db
