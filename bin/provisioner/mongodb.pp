799709024f7dfc5e5073a2181348a99b36aceda9
liquid-rc04 <liquid-rc04@liquid>
2015-10-11 21:20:50 -0400

    provisoiner:  - mongodb passes

--- bin/provisioner/mongodb.pp
+++ bin/provisioner/mongodb.pp
@@ -1,24 +1,26 @@
 
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
 class mongodb {
-#export LC_ALL=C
 
 # source: http://stackoverflow.com/questions/1139127/how-to-trust-a-apt-repository-debian-apt-get-update-error-public-key-is-not-av
 #apt-get install debian-keyring
 #gpg --keyserver pgp.mit.edu --recv-keys 9ECBEC467F0CEB10
 #gpg --armor --export 9ECBEC467F0CEB10 | apt-key add -
-    Exec { path => "/usr/bin" }
     exec { "reconfigure":
 	command => "sudo dpkg-reconfigure -phigh -a",
         timeout => 60,
         tries   => 3,
         before  => Exec["mongo purge"],
+	returns => [0, 1],
     }
     exec { "mongo purge":
-	#command => "sudo /usr/binapt-get purge -y mongodb-org",
-	command => "sudo /usr/bin/apt-get purge -y mongodb-server",
+	command => "sudo /usr/binapt-get purge -y mongodb-org",
+	#command => "sudo /usr/bin/apt-get purge -y mongodb-server",
         timeout => 60,
         tries   => 3,
         before  => Exec["mongo autoremove"],
+	returns => [0, 1],
     }
     exec { "mongo autoremove":
 	command => "sudo /usr/bin/apt-get autoremove -y",
@@ -31,6 +33,7 @@ class mongodb {
         timeout => 60,
         tries   => 3,
         before  => Exec["mongo add deb sources"],
+	returns => [0, 1],
     }
     exec { "mongo add deb sources":
 	command => "echo 'deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main' | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list",
@@ -45,8 +48,14 @@ class mongodb {
         before  => Exec["mongo aptget install"],
    }
     exec { "mongo aptget install":
-	#command => "sudo apt-get install -y --force-yes mongodb-org",
-	command => "sudo apt-get install -y --force-yes mongodb-server",
+	command => "sudo apt-get install -y --force-yes mongodb-org",
+	#command => "sudo apt-get install -y --force-yes mongodb-server",
+        timeout => 60,
+        tries   => 3,        
+        before  => Exec["exportlocale"],
+    }
+    exec { "exportlocale":
+	command => "echo 'export LC_ALL=C' >> /root/.bashrc; echo 'export LC_ALL=C' >> /home/qore/.bashrc",
         timeout => 60,
         tries   => 3,        
     }
