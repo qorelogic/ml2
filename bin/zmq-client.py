4299a44068b275fd2b82a38acba0fe6048738788
jeff kiiza <kiizaa@gmail.com>
2015-10-28 15:24:44 -0300

    currencyMatrix:  - ncurses refreshes with datafeed updates

--- bin/zmq-client.py
+++ bin/zmq-client.py
@@ -703,13 +703,19 @@ stdscr.keypad(1)
 curses.curs_set(0)
 curses.mousemask(1)
 
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.keypad(1)
+
+mode = sys.argv[2]
+zc = ZMQClient()
+
 try:
     mode = sys.argv[2]
     zc = ZMQClient()
     zc.client(mode=mode)
 except KeyboardInterrupt as e:
-    curses.nocbreak(); stdscr.keypad(0); curses.echo()
-    curses.endwin()
     print ''
 except Exception as e:
     curses.nocbreak(); stdscr.keypad(0); curses.echo()
@@ -719,6 +725,11 @@ except Exception as e:
     #qd.on()
     #qd.printTraceBack()
     sys.exit(0)
+
+curses.nocbreak(); 
+stdscr.keypad(0); 
+curses.echo()
+curses.endwin()
 
 curses.nocbreak(); 
 stdscr.keypad(0); 
