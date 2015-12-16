ca7f010e0e53848cccd06827e2edcda67719bc28
jeff kiiza <kiizaa@gmail.com>
2015-12-16 15:41:58 -0300

    setups:

---
+++ bin/setups/setup.zmq-client.sh
@@ -0,0 +1,116 @@
+########## zmq-client
+cd /mldev/bin
+ls
+.gb
+git checkout forecaster.refactored-0.3
+git reset --hard 
+git reset --hard 
+git stash drop stash@{0} 
+git stash drop stash@{0} 
+git stash drop stash@{0} 
+git stash drop stash@{0} 
+git fetch origin 
+.gb
+.gpull
+mx w 1broker
+./datafeeds/setup.sh 
+.gb
+git rebase forecaster.refactored-0.3 forecaster.refactored-0.3-currencyMatrix-ncurses4 
+.gai
+nano oandaq.py
+.gai
+git rebase --continue 
+git rebase --skip 
+git fetch origin forecaster.refactored-0.3-currencyMatrix-ncurses4:forecaster.refactored-0.3-currencyMatrix-ncurses4 
+git checkout forecaster.refactored-0.3-currencyMatrix-ncurses4
+git rebase forecaster.refactored-0.3 forecaster.refactored-0.3-currencyMatrix-ncurses4 
+./datafeeds/setup.sh 
+ls ../lib/bitcoin/1broker/aol1306_1broker-trading-API.github.py.git/
+cd "/home/qore"
+clear
+cd /ml.dev/bin
+python zmq-client.py 104.236.64.84:5555 avg
+.gb
+.gba
+.gpull
+python zmq-client.py 104.236.64.84:5555 avg
+killall -s SIGKILL python
+python zmq-client.py 104.236.64.84:5555 avg
+killall -s SIGKILL python
+python zmq-client.py 104.236.64.84:5555 avg
+killall -s SIGKILL python
+python zmq-client.py 104.236.64.84:5555 avg
+killall -s SIGKILL python
+python zmq-client.py 104.236.64.84:5555 avg
+ps aux
+ps aux | grep -i zmq
+ps aux | grep -i zmq -
+#kill -9 1242 1704 1741 1900 1919 1959
+ps aux | grep -i zmq -
+#kill -9 19432
+ps aux | grep -i zmq -
+python zmq-client.py 104.236.64.84:5555 avg
+killall -s SIGKILL python
+python zmq-client.py 104.236.64.84:5555 avg
+killall -s SIGKILL python
+python zmq-client.py 104.236.64.84:5555 avg
+killall -s SIGKILL python
+python zmq-client.py 104.236.64.84:5555 avg
+killall -s SIGKILL python
+cd "/home/qore"
+clear
+python main.py
+cd /mldev/lib/bitcoin/1broker/aol1306_1broker-trading-API.github.py.git/
+ls
+python main.py
+python main.py
+nano main.py 
+python main.py
+cd "/home/qore"
+clear
+python main.py
+pwd
+cd /mldev/lib/bitcoin/1broker/
+python main.py
+cd aol1306_1broker-trading-API.github.py.git/
+python main.py
+nano settings.json 
+python main.py
+nano settings.json 
+python main.py
+nano settings.json 
+python main.py
+nano main.py 
+python main.py
+nano main.py 
+python main.py
+[A
+python main.py
+nano main.py
+python main.py
+ifconfig 
+nano settings.json 
+nano main.py
+python main.py
+nano settings.json 
+python main.py
+nano settings.json 
+python main.py
+nano settings.json 
+python main.py
+cd /mldev/bin
+ls
+python zmq-client.py 104.236.64.84:5555 spread
+[A
+python zmq-client.py 104.236.64.84:5555 spread
+cd /mldev/bin/
+nano provisioner/default.pp 
+sudo puppet apply provisioner/default.pp 
+.gb
+.gai
+nano provisioner/default.pp 
+sudo puppet apply provisioner/default.pp 
+nano provisioner/default.pp 
+cat ~/.ssh/id_rsa.pub 
+python 
+########## end zmq-client
