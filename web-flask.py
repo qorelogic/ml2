36be8af570e47ba7456203c76da719b73a31ed5a
jeff kiiza <kiizaa@gmail.com>
2016-01-09 06:20:26 -0300

    openflights:  - all commercial airline routes

--- web-flask.py
+++ web-flask.py
@@ -25,6 +25,30 @@ hdir = 'lib/crawlers/transport/jpatokal_openflights.github.py.git/data'
 
 app = Flask(__name__)
 
+def getRoutes():
+    
+    dfa = p.read_csv(hdir+'/airports.dat', header=None)
+    dfaa = dfa.ix[:,4]
+    ldfaa = list(dfaa)
+    
+    dfr = p.read_csv(hdir+'/routes.dat', header=None)
+    airportCodesFrom = list(dfr.ix[:, 2])
+    airportCodesTo   = list(dfr.ix[:, 4])
+    data = []
+    #for i in xrange(len(dfr.index)):
+    for i in dfr.index:
+        try:
+            airportCodeFrom = ldfaa.index(airportCodesFrom[i])
+            airportCodeTo   = ldfaa.index(airportCodesTo[i])
+            #print '{0} {1}'.format(dfa.ix[airportCodeFrom, 6], dfa.ix[airportCodeFrom, 7])
+            #print '{0} {1}'.format(dfa.ix[airportCodeTo, 6], dfa.ix[airportCodeTo, 7])
+            data.append([[dfa.ix[airportCodeFrom, 6], dfa.ix[airportCodeFrom, 7]], [dfa.ix[airportCodeTo, 6], dfa.ix[airportCodeTo, 7]]])
+        except Exception as e:
+            #print e
+            ''
+    #print p.DataFrame(data)
+    return data
+  
 @app.route("/")
 @app.route("/hello")
 @app.route('/hello/<name>')
@@ -32,21 +56,23 @@ def hello(name=None):
     #an = Feeder()
     #an.fireupThreads()  
     
-    df = p.read_csv(hdir+'/airports.dat', header=None)
-    #print df.ix[0, :].get_values()
-    data = df.ix[:,[6,7]].get_values()
-    #print data 
-    #print df
+    dfa = p.read_csv(hdir+'/airports.dat', header=None)
+    dfr = p.read_csv(hdir+'/routes.dat', header=None)
+    #print dfa.ix[0, :].get_values()
+    airports = dfa.ix[:,[6,7]].get_values()
+    #print airports 
+    #print dfa
     #gmaps.heatmap?
-    #gmaps.heatmap(data, height='400px', width='980px', max_intensity=2, point_radius=8)
-    jdata = j.dumps(data.tolist())
+    #gmaps.heatmap(airports, height='400px', width='980px', max_intensity=2, point_radius=8)
+    airports = j.dumps(airports.tolist())
+    routes   = getRoutes()
     
     #data = gmaps.datasets.load_dataset('taxi_rides')
     #maps = gmaps.heatmap(data)
     #gmaps.display(maps)
 
-    return render_template('hello.html', name=name, data=jdata)    
+    return render_template('hello.html', name=name, airports=airports, routes=routes)    
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=80)
