4983b39de856c5fcdc1e61e11e080839e5602c55
jeff kiiza <kiizaa@gmail.com>
2016-01-31 01:10:48 -0300

    provisioner:  - datafeeds

--- bin/provisioner/system-update.pp
+++ bin/provisioner/system-update.pp
@@ -4,7 +4,9 @@ class system-update {
     command => '/usr/bin/apt-get update',
   }
 
-  $sysPackages = [ "build-essential", "htop", 'iotop', 'python-pip', 'python-dev' ]
+  # sudo apt-get install python-scipy
+  # python packages for datafeeds: python-matplotlib
+  $sysPackages = [ "build-essential", "htop", 'iotop', 'python-pip', 'python-dev',  "python-scipy", 'ipython', 'ipython-notebook', 'python-matplotlib', 'python-tk', 'python-pil' ]
   package { $sysPackages:
     ensure => "installed",
     require => Exec['apt-get update'],
