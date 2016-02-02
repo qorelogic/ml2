25c3db6c3c4196f48bed2072366233a931cd87d9
jeff kiiza <kiizaa@gmail.com>
2016-02-01 22:53:03 -0300

    vpn:  - mod2

--- bin/provisioner/vpn.pp
+++ bin/provisioner/vpn.pp
@@ -4,8 +4,7 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 class vpn-server {
     package { "pptpd":
     ensure  => present,
-    #require => Class["system-update"],
-    
+    require => Class["system-update"],
     }
     #$qwe = generate("/bin/echo", "-n", "Hallo Welt")
     # source: https://groups.google.com/forum/#!topic/puppet-users/HXE4w_TlulM
@@ -51,6 +50,35 @@ class vpn-server {
     #	command => "iptables -t nat -A POSTROUTING -o venet0:0 -j MASQUERADE && iptables-save",
     	command => "iptables -t nat -A POSTROUTING -o $miface -j MASQUERADE && iptables-save",
     }
+
+#    exec { "p1": before => Exec["p2"], command => "perl -pi -e 's/.*(localip).*//g' /etc/pptpd.conf"}
+#    exec { "p2": before => Exec["p3"], command => "echo 'localip 10.0.0.1' >> /etc/pptpd.conf"}
+
+#    exec { "p3": before => Exec["p4"], command => "perl -pi -e 's/.*(remoteip).*//g' /etc/pptpd.conf"}
+#    exec { "p4": before => Exec["p5"], command => "echo 'remoteip 10.0.0.100-200' >> /etc/pptpd.conf"}
+
+#    exec { "p5": before => Exec["p6"], command => "echo 'ms-dns 8.8.8.8' >> /etc/pptpd.conf"}
+
+#    exec { "p6": before => Exec["p7"], command => "perl -pi -e 's/.*(box1 ).*//g' /etc/chap-secrets" }
+#    exec { "p7": before => Exec["p8"], command => "echo 'box1 pptpd qweqwe1' >> /etc/chap-secrets"}
+
+#    exec { "p8": before => Exec["p9"],  command => "perl -pi -e 's/.*(ms-dns ).*//g' /etc/ppp/pptpd-options"}
+#    exec { "p9": before => Exec["p10"], command => "echo 'ms-dns 8.8.8.8' >> /etc/ppp/pptpd-options"}
+#    exec { "p10": before => Exec["p11"],   command => "echo 'ms-dns 8.8.4.4' >> /etc/ppp/pptpd-options" }
+
+#    exec { "p11": before => Exec["p12"],   command => "service pptpd restart" }
+
+#    exec { "p12": before => Exec["p13"],   command => "perl -pi -e 's/net.ipv4.ip_forward = .*/net.ipv4.ip_forward = 1/g' "}
+#    exec { "p13": before => Exec["p14"],   command => "sysctl -p"}
+#    exec { "p14": before => Exec["   "],   command => "iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE && iptables-save"}
+
+#perl -pi -e 's/.*(localip).*/\1 10.0.0.1/g' /etc/pptpd.conf
+#perl -pi -e 's/.*(remoteip).*/\1 10.0.0.100-200/g' /etc/pptpd.conf
+#echo 'box1 pptpd qweqwe1' >> /etc/chap-secrets
+
+#perl -pi -e 's/#(ms-dns ).*/\1 8.8.8.8/g' /etc/pptpd.conf
+#perl -pi -e 's/#(ms-dns ).*/\1 8.8.4.4/g' /etc/pptpd.conf
+
 }
 
 import 'system-update.pp'
