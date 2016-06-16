27d0542356467a7d8cb86dc99d9e1d7467de2f0c
jeff kiiza <kiizaa@gmail.com>
2016-06-16 12:39:47 -0300

    mod

--- bin/qoreliquid.py
+++ bin/qoreliquid.py
@@ -1858,7 +1858,7 @@ def rebalanceTrades(dfu3, oanda2, accid, dryrun=True, leverage=50, verbose=False
                         ans = raw_input('Sure you want to create order? (y/N): ')
                         if ans != 'y':
                             raise(Exception('User intervened: order not created'))
-                    if noInteractiveLeverage OR noInteractiveDeleverage:
+                    if noInteractiveLeverage == True OR noInteractiveDeleverage == True:
                         noInteractive == True
                     print 'noInteractive:           %s' % noInteractive
                     print 'noInteractiveLeverage:   %s' % noInteractiveLeverage
