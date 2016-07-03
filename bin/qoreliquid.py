915dfe4b1089e71c37b541249efb883029facc6f
jeff kiiza <kiizaa@gmail.com>
2016-07-03 20:05:09 -0300

    mod3 trades

--- bin/qoreliquid.py
+++ bin/qoreliquid.py
@@ -1831,9 +1831,8 @@ def rebalanceTrades(dfu3, oanda2, accid, dryrun=True, leverage=50, verbose=False
                 print len(currentTrades)
                 #print currentTrades.sort(['instrument', 'id'], ascending=[True, True]).set_index('id').ix[:,'instrument price side time units'.split(' ')]
                 ct = currentTrades.set_index('id').ix[:,'instrument price side sideBool units ask bid plpips pl sideS status time displayName maxTradeUnits pip'.split(' ')]
-                ct = ct.sort('pl', ascending=False)
-                print ct[ct['pl'] > 0]
-                print ct[ct['pl'] < 0]
+                print ct.sort('pl', ascending=False).[ct['pl'] > 0]
+                print ct.sort('pl', ascending=True)[ct['pl'] < 0]
                 print 'currentPositions:'
                 print currentPositions.sort('units', ascending=False)
 
