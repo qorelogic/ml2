5ff0c785d7ee3e49fba215410140a8b5e981d4d8
Jeff Kiiza <kiizaa@gmail.com>
2017-01-08 22:24:55 -0300

    mod logTraceBack

--- bin/patterns.py
+++ bin/patterns.py
@@ -103,7 +103,7 @@ try:
             accid = acc.ix[accountIndex, 'accountId']
         except Exception as e:
             print e
-            qd.logTraceBack()
+            qd.logTraceBack(e)
             sys.exit()
     if int(args.verbose) >= 1:
         print 'using account: {0}'.format(accid)
@@ -313,7 +313,7 @@ def main(args, leverage=10, dryrun=True, verbose=False):
             dfu33 = rebalanceTrades(oq, dfu2, oanda0, str(args.account), dryrun=dryrun, leverage=leverage, verbose=verbose, noInteractive=noInteractive, noInteractiveLeverage=noInteractiveLeverage, noInteractiveDeleverage=noInteractiveDeleverage, noInteractiveFleetingProfits=noInteractiveFleetingProfits, sortRebalanceList=sortRebalanceList, loginIndex=loginIndex)
         except oandapy.OandaError as e:
             print e
-            qd.logTraceBack()
+            qd.logTraceBack(e)
             print 'Try a different account number'
     else:
         dfu33 = rebalanceTrades(oq, dfu2, oanda0, accid, dryrun=dryrun, leverage=leverage, verbose=verbose, noInteractive=noInteractive, noInteractiveLeverage=noInteractiveLeverage, noInteractiveDeleverage=noInteractiveDeleverage, noInteractiveFleetingProfits=noInteractiveFleetingProfits, threading=threading, sortRebalanceList=sortRebalanceList, loginIndex=loginIndex)
@@ -427,7 +427,7 @@ if __name__ == "__main__":
     try:
         main(args, leverage=leverage, dryrun=dryrun)
     except Exception as e:
-        qd.logTraceBack()
+        qd.logTraceBack(e)
         print e
     """
     
@@ -599,7 +599,7 @@ q  = quit
             else:
                 main(args, leverage=leverage, dryrun=dryrun)
         except Exception as e:
-            qd.logTraceBack()
+            qd.logTraceBack(e)
             print e
         if not args.interactive:
             break
