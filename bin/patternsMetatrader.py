0e441962ccd167783007c11e174950370d451773
Jeff Kiiza <kiizaa@gmail.com>
2017-05-21 19:31:40 -0300

    patternsMetatrader: -min --minimal argument

--- bin/patternsMetatrader.py
+++ bin/patternsMetatrader.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 #%reload_ext autoreload
 #%autoreload 2
+import argparse
+# source: https://docs.python.org/2/howto/argparse.html
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", '--verbose', help="turn on verbosity")
+parser.add_argument("-b", '-bal', '--balance', help="turn on verbosity")
+parser.add_argument("-l", '-lev', '--leverage', help="turn on verbosity")
+parser.add_argument("-min", '--minimal', help="", action="store_true")
+args = parser.parse_args()
+
 import sys
 def defp(pt):
     try:    sys.path.index(pt)
@@ -21,17 +30,17 @@ def main():
     
     # compute metatrader portfolio
     def usage():
-        print 'usage: <balance> <leverage>'
+        print 'usage: -b <balance> -l <leverage> [-min]'
     try:
-        balance  = float(sys.argv[1])
+        balance  = float(args.balance)
     except Exception as e:
-        print e
+        #print e
         usage()
         sys.exit()
     try:
-        leverage = int(sys.argv[2])
+        leverage = int(args.leverage)
     except Exception as e:
-        print e
+        #print e
         usage()
         sys.exit()
     try:
@@ -47,8 +56,11 @@ def main():
     df = p.read_csv(fname, index_col=[0])
     df = pa.computePortfolioMetatrader(df, balance=balance, leverage=leverage, method='etoro')
     with p.option_context('display.max_rows', 4000, 'display.max_columns', 4000, 'display.width', 1000000):
-        fields = 'side lots lotsEtoro _lotsEtoroRT _lotsEtoroRT2 _diffpLotsEtoro _minimumLeverageEtoro2 minimumLeverageEtoro unitsEtoro amount2Metatrader diffp lots'.split(' ')
-        #fields = 'side lots lotsEtoro minimumLeverageEtoro unitsEtoro balanceMetatrader leverageMetatrader allMarginMetatrader amount2Metatrader closeTradePLMetatrader side diffp lots'.split(' ')
+        if args.minimal:
+            fields = 'side lots lotsEtoro minimumLeverageEtoro unitsEtoro amount2Metatrader side diffp lots'.split(' ')
+            #fields = 'side lots lotsEtoro minimumLeverageEtoro unitsEtoro balanceMetatrader leverageMetatrader allMarginMetatrader amount2Metatrader closeTradePLMetatrader side diffp lots'.split(' ')
+        else:
+            fields = 'side lots lotsEtoro _lotsEtoroRT _lotsEtoroRT2 _diffpLotsEtoro _minimumLeverageEtoro2 minimumLeverageEtoro unitsEtoro amount2Metatrader diffp lots'.split(' ')
         df = df.ix[:, fields]
         #df = df.sort_index()
         df = df.sort_values(by='diffp', ascending=False)
