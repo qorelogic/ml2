45f34038a0a714b60c41fda9a7f8587c4291add9
Jeff Kiiza <kiizaa@gmail.com>
2017-07-22 17:33:07 -0300

    0016 Crypto Meesage Queues: initial routines

---
+++ bin/coins/bittrex.websockets.subscribe.producer.js
@@ -0,0 +1,27 @@
+
+// producer.js
+var zmq = require('zeromq')
+  , sock = zmq.socket('push');
+
+sock.bindSync('tcp://127.0.0.1:3000');
+console.log('Producer bound to port 3000');
+
+var i = 1;
+
+setInterval(function(){
+  console.log('sending work');
+  sock.send('some work '+i);
+  i++;
+}, 10);
+/*
+// worker.js
+var zmq = require('zeromq')
+  , sock = zmq.socket('pull');
+
+sock.connect('tcp://127.0.0.1:3000');
+console.log('Worker connected to port 3000');
+
+sock.on('message', function(msg){
+  console.log('work: %s', msg.toString());
+});
+*/
