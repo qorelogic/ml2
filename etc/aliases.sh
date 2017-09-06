d9086f98446c8bbe6ece2d892b61f77fbe2659f9
Jeff Kiiza <kiizaa@gmail.com>
2017-09-06 14:18:27 -0300

    aliases .gpush: git push --tags

--- etc/aliases.sh
+++ etc/aliases.sh
@@ -130,6 +130,7 @@ alias sc='scrapy'
 		goption=""
 	fi
 	git push $goption origin $currentBranch:$currentBranch
+	git push $goption --tags origin
 }
 .gpull() {
 	currentBranch="`git branch | grep '\*' | cut -d':' -f3 | cut -c 3-`"                                                       
