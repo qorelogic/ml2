b69b867c7e041aa16c0cdd8e5d57b26fcedae135
Jeff Kiiza <kiizaa@gmail.com>
2017-10-13 17:20:28 -0300

    vimrc: python ide

--- bin/functions.sh
+++ bin/functions.sh
@@ -88,3 +88,11 @@ swapOff() {
 	fi
 }
 
+vimInstall() {
+	rm -f ~/.inputrc
+	ln -s /mldev/etc/inputrc ~/.inputrc
+	rm -f  ~/.vimrc
+	ln -s /mldev/etc/vimrc   ~/.vimrc
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+}
+
