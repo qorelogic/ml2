d15f6710ba5634683c549595f296eb86176f4f3d
Jeff Kiiza <kiizaa@gmail.com>
2018-01-20 00:28:23 -0300

    mod

--- bin/solidity/web3.node.js
+++ bin/solidity/web3.node.js
@@ -3,14 +3,9 @@ Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 //console.log(web3);
 
-web3.eth.getBalance(web3.eth.accounts[1])
-
-//from = web3.eth.getBalance(web3.eth.accounts[1]);
-
-eth = {'to':web3.eth.accounts[1], 'from': web3.eth.accounts[0]};
-
+eth = {'from': web3.eth.accounts[0], 'to':web3.eth.accounts[1]};
 //console.log(eth);
 
+// source: https://ethereum.stackexchange.com/questions/29064/send-ether-to-contract-address-in-web3-js
 var send = web3.eth.sendTransaction({from:eth.from, to:eth.to, value:web3.toWei(0.05, "ether")});
-
 console.log(web3.eth.getBalance(web3.eth.accounts[1])['c'][0]);
